f826882d6699567507a9f7944d84981f480a3269
sugita-seiya <satreu16@ezweb.ne.jp>
2020-01-24 10:05:17 +0900

    モデルテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true, length: { maximum: 6}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :posts
   has_many :comments
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it "nicknameが空では登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "emailが空では登録できないこと"do
+      user = build(:user,email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "passwordが空では登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
+      user = build(:user, password_confirmation: nil)
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "nicknameが7文字以上であれば登録できないこと" do
+      user = build(:user, nickname: "aaaaaaaa")
+      user.valid?
+      expect(user.errors[:nickname]).to include("is too long (maximum is 6 characters)")
+    end
+    
+    it "nicknameが6文字以下では登録できること" do
+      user = build(:user, nickname: "aaaaaa")
+      expect(:user).to be_valid
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+
+    it "passwordが6文字以上であれば登録できること" do
+      
+    end
+  end
+end

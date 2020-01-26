d3983c9f6fe8a6b8640c0127c19a64ba75ca427f
sugita-seiya <satreu16@ezweb.ne.jp>
2020-01-26 12:39:58 +0900

    searchesのルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   root to: "top#index"
   devise_for :users
-  # namespace :posts do
-  #   resources :searches, only: :index
-  # end
+  namespace :posts do
+    resources :searches, only: :index
+  end
   resources :posts do
     resources :comments, only: :create
-    collection do
-      get 'search'
-    end
     namespace :api do
       resources :comments, only: :index, defaults: { format: 'json' }
     end

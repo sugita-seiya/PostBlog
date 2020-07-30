cc37e0db7c381445932f994b857f83ab4d6a311a
sugita-seiya <satreu16@ezweb.ne.jp>
2020-07-31 05:19:06 +0900

    ロジック変更

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -6,6 +6,7 @@ class Api::CommentsController < ApplicationController
     last_comment_id = params[:id].to_i
     # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
     @comments = post.comments.includes(:user).where('id > ?', last_comment_id)
+    
   end
 end
 

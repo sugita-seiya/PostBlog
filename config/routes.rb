c98dc07802f414cb2cb514b9658baa936126df35
sugita-seiya <satreu16@ezweb.ne.jp>
2020-08-02 07:14:00 +0900

    ロジック変更

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :comments, only: :create
     namespace :api do
       resources :comments, only: :index, defaults: { format: 'json' }
+      
     end
   end
   resources :users, only: :show
